7fa5b3b84c5840ed2123b3ac441a43306b15c88a
DiabolicalKitten <amandaperron126@gmail.com>
2026-04-24 12:46:52 -0400

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,10 @@
 # 4/24/26
 # A simple Python program to print a message
 
-print("Hello, Wolrd!")
+
+#Amanda Perron
+# 4/24/26
+# added new message
+
+print("Hello, World!")
+print("Welcome to GitHub!")
